5a601130fa4bbb1f6851dcf211e155d3bec702e6
Seba Ruiz <seba.lruiz@gmail.com>
2018-10-26 11:43:43 -0300

    Detalle del Rol

--- login v6/Controllers/RoleController.cs
+++ login v6/Controllers/RoleController.cs
@@ -10,9 +10,9 @@ using Negocio.Dominio;
 
 namespace login_v6.Controllers
 {
+   [Authorize]
     public class RoleController : Controller
     {
-        //private ApplicationRoleManager _roleManager;
 
         public RoleController(){}
 
@@ -26,82 +26,15 @@ namespace login_v6.Controllers
             return View(roles);
         }
 
-        //public RoleController(ApplicationRoleManager roleManager)
-        //{
-
-        //    RoleManager = roleManager;
-        //}
-
-        //public ApplicationRoleManager RoleManager
-        //{
-        //    get
-        //    {
-        //        return _roleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
-        //    }
-        //    private set
-        //    {
-        //        _roleManager = value;
-        //    }
-        //}
-        //// GET: Role
-        //public ActionResult Index()
-        //{
-        //    List<RoleViewModel> list = new List<RoleViewModel>();
-        //    foreach (var role in RoleManager.Roles)
-        //        list.Add(new RoleViewModel(role));
-
-        //    return View(list);
-        //}
-
-        //public ActionResult Create()
-        //{
-
-        //    return View();
-
-        //}
-        //[HttpPost]
-        //public async Task<ActionResult> Create (RoleViewModel model)
-        //{
-
-        //    var role = new ApplicationRole() { Name = model.nombre };
-        //    await RoleManager.CreateAsync(role);
-        //    return RedirectToAction("Index");
-        //}
-
-        //public async Task<ActionResult> Edit(string Id)
-        //{
-        //    var role = await RoleManager.FindByIdAsync(Id);
-        //    return View(new RoleViewModel(role));
-        //}
-        //[HttpPost]
-        //public async Task<ActionResult> Edit(RoleViewModel model)
-        //{
-        //    var role = new ApplicationRole() { Id = model.Id, Name = model.nombre };
-        //    await RoleManager.UpdateAsync(role);
-        //    return RedirectToAction("Index");
-        //}
+        public ActionResult Detalle()
+        {
+            return View();
+        }
 
 
-        //public async Task<ActionResult> Detail(string Id)
-        //{
-        //    var role = await RoleManager.FindByIdAsync(Id);
-        //    return View(new RoleViewModel(role));
 
-        //}
 
-        //public async Task<ActionResult> Delete(string Id)
-        //{
-        //    var role = await RoleManager.FindByIdAsync(Id);
-        //    return View(new RoleViewModel(role));
-        //}
-        //[HttpPost]
-        //public async Task<ActionResult> DeleteConfirmed(string Id)
-        //{
-        //    var role = await RoleManager.FindByIdAsync(Id);
-        //    await RoleManager.DeleteAsync(role);
 
-        //    return RedirectToAction("Index");
-        //}
 
 
 
