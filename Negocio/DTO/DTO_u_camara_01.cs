84838882faa77e40d7778bfca1d78bdd4b18da49
Seba Ruiz <seba.lruiz@gmail.com>
2018-11-04 19:47:29 -0300

    logica de camara terminada

---
+++ Negocio/DTO/DTO_u_camara_01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.DTO
+{
+    public class DTO_u_camara_01
+    {
+        public string nro_ip { get; set; }
+        public string descripcion { get; set; }
+
+        public int id_camara { get; set; }
+        public string mmarca { get; set; }
+        public DateTime? fecha_ubicacion { get; set; }
+        public string nombre_ubi { get; set; }
+        public int id_detalle { get; set; }
+    }
+}
---
+++ Negocio/DTO/DTO_u_detalle_camara.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.DTO
+{
+    public class DTO_u_detalle_camara
+    {
+        public string nro_ip { get; set; }
+        public string descripcion { get; set; }
+        public int id_camara { get; set; }
+        public int id_mm { get; set; }
+        public DateTime? fecha_ubicacion { get; set; }
+        public int id_ubicacion_camara { get; set; }
+        public int id_detalle { get; set; }
+
+
+    }
+}
